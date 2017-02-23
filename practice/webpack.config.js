5b4fdbfc5c62fa1b2cff1df2951c3665eb4a3fa2
zhoulin168 <lin.zhou@newband.com>
2017-02-23 17:20:12 +0800

    add

---
+++ practice/webpack.config.js
@@ -0,0 +1,35 @@
+/**
+ * Created by 周林 on 2017/2/22.
+ */
+var path =require("path");
+var webpack =require("webpack");
+
+module.exports ={
+    entry:[
+        'webpack-dev-server/client?http://0.0.0.0:8080',
+        'webpack/hot/only-dev-server',
+        path.resolve(__dirname,"./app/main.js")],
+    output:{
+        path:path.resolve(__dirname,'./build'),
+        filename:'bundle.js',
+        publicPath:'http://localhost:8080/build/'
+    },
+    module:{
+        loaders:[
+            {test:/\.css$/,loaders:"style-loader!css-loader"},
+            {
+                test:/\.(js|jsx)$/,
+                exclude:/node_modules/,
+                loaders:["jsx-loader"],
+
+            }
+        ]
+    },
+    resolve:{
+        extensions:['.js','.json']
+    },
+    plugins:[
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin()
+    ]
+}
