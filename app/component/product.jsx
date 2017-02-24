98ce2a8fce53207f0f62c5608d76c35f5c221aa0
NEWBAND <lin.zhou@newband.com>
2017-02-24 17:42:38 +0800

    add some

--- app/component/product.jsx
+++ app/component/product.jsx
@@ -1,15 +1,33 @@
 // var React = require('react');
 import React,{Component} from "react";
 import "../css/ReactTrans.css";
-import ReactCSSTransitionGroup from "react-addons-css-transition-group"
-// require("../css/ReactTrans.css");
-// var ReactCSSTransitionGroup =require('react-addons-css-transition-group');
+
+class Page1 extends React.Component{
+    constructor(props){
+        super(props)
+        this.state={saved:false}
+    }
+    changeColor(){
+        console.log(1);
+        this.setState({
+            saved:true
+        })
+    }
+    render(){
+        let {save,text} = this.props;
+        return(
+
+            <div>
+               <h1 onClick={this.changeColor.bind(this)}>{this.state.saved?save:text} aaa </h1>
+            </div>
+        )
+    }
+}
+
 class Product extends React.Component{
     render(){
         return(
-            <div>
-              what
-            </div>
+            <Page1/>
         )
     }
 }
